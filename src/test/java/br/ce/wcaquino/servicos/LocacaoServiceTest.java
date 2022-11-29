fcfbc434685684cf10580d4b3b3b2695a04bd45b
Ariane01 <arianebarboza2003@hotmail.com>
2022-11-29 15:09:40 -0300

    Aula Assertivas falando sobre ordem no teste

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -13,55 +13,21 @@ import java.util.Date;
 
 public class LocacaoServiceTest {
 
-    @Test
-    public static void teste1() throws Exception {
-        //cenario
-        // Cenário: Onde as variáveis serão inicializadas
-
-        LocacaoService service = new LocacaoService();
-        Usuario usuario = new Usuario("Usuario 1");
-        Filme filme = new Filme("Filme 1", 2, 5.0);
-
-        //acao
-        //Ação onde vamos invocar o método que vamos testar
-
-        Locacao locacao = service.alugarFilme(usuario, filme);
-
-        //verificacao
-        // Validação, onde vamos coletar os resultados da ação com aquele
-        // cenário especificado e podemos avaliar se o resultado está
-        // de acordo ao esperado.
-
-        // Aqui eu verifico se o valor da alocação é igual a 5
-        Assert.assertTrue(locacao.getValor() == 4.0);
-
-        //Aqui verifico se a data da alocação é a mesma data atual
-        Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-
-        // A data de retorno deve ser a data do dia seguinte, quantos dias eu quero além da data do dia de hoje
-        Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
-    }
-
-    @Test
-    public void testeLocacao(){
+       @Test
+    public void testeLocacao() throws Exception {
 
         //cenário
         LocacaoService service = new LocacaoService();
         Usuario usuario = new Usuario("Usuario 1");
-        Filme filme = new Filme ("Filme1" , 2, 5.0);
+        Filme filme = new Filme ("Filme1" , 2, 4.0);
 
         //ação
 
-        Locacao locacao = null;
-        try {
-            locacao = service.alugarFilme(usuario, filme);
-        } catch (Exception e) {
-             e.printStackTrace();
-        }
+       Locacao locacao = service.alugarFilme(usuario, filme);
 
         // verificação
 
-
+        Assert.assertEquals(4.0, locacao.getValor(), 0.01);
         Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
         Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
 
