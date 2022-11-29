6e20f061743d1d3804b3bd959aa7458a81ed7684
Ariane01 <arianebarboza2003@hotmail.com>
2022-11-29 15:34:06 -0300

    Aula 06 assunto AssertThat

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -4,38 +4,47 @@ import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
 import br.ce.wcaquino.utils.DataUtils;
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Date;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 
 public class LocacaoServiceTest {
 
-       @Test
-    public void testeLocacao() throws Exception {
+    @Test
+    public void teste() throws Exception {
 
         //cenário
         LocacaoService service = new LocacaoService();
         Usuario usuario = new Usuario("Usuario 1");
-        Filme filme = new Filme ("Filme1" , 2, 4.0);
+        Filme filme = new Filme("Filme1", 2, 4.0);
 
         //ação
 
-       Locacao locacao = service.alugarFilme(usuario, filme);
+        Locacao locacao = service.alugarFilme(usuario, filme);
 
         // verificação
 
-        Assert.assertEquals(4.0, locacao.getValor(), 0.01);
-        Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-        Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+        //valor da alocação é 5
+        assertThat(locacao.getValor(), is(5.0));
+        // Valor da alocação não é 6
+        assertThat(locacao.getValor(), is(not(6.0)));
+        assertThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+        assertThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
 
 
     }
 
-
+    private Object not(double v) {
+        return null;
     }
+}
+
+
+
 
 
 
